85f49d3a26a7ed1ab232bda3e848433e7b073ea5
ShivaniElitem <SXE210018@utdallas.edu>
2023-03-25 13:44:20 -0500

    Cat images and Random feature

--- app/src/main/java/com/example/randompet/MainActivity.kt
+++ app/src/main/java/com/example/randompet/MainActivity.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     var petImageURL = ""
@@ -49,7 +50,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun getNextImage(button: Button, imageView: ImageView) {
         button.setOnClickListener {
-            getDogImageURL()
+            var choice = Random.nextInt(2)
+
+            if (choice == 0) {
+                getDogImageURL()
+            }
+            else {
+                getCatImageURL()
+            }
 
             Glide.with(this)
                 . load(petImageURL)
@@ -57,4 +65,25 @@ class MainActivity : AppCompatActivity() {
                 .into(imageView)
         }
     }
+
+    private fun getCatImageURL() {
+        val client = AsyncHttpClient()
+
+        client["https://api.thecatapi.com/v1/images/search", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful$json")
+                var resultsJSON = json.jsonArray.getJSONObject(0)
+                petImageURL = resultsJSON.getString("url")            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+
 }
